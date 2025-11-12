fd99b7249820bebf1a4c710220e354cab6a3f2d3
EroInfernoTeam <eroinfernoteam@gmail.com>
2025-11-12 17:01:39 -0300

    1234

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762976404|126853612';
+const CACHE_VERSION = '1762977647|1369773837';
 /** @type {string} */
 const CACHE_PREFIX = 'Supa Loboto X Pr-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
