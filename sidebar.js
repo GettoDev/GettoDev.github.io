45dab071920a26def14887e22f20c295e7c85c27
EroInfernoTeam <eroinfernoteam@gmail.com>
2025-11-25 06:24:14 -0300

    final

--- sidebar.js
+++ sidebar.js
@@ -1,38 +1,25 @@
-const sections = [
-  {
-    title: ' ',
-    links: ['About Us', 'Guestbook']
-  },
-  {
-    title: 'Game Lab',
-    links: ['Pies Frescos', 'UnderConstruction', 'UnderConstruction']
-  },
-  {
-    title: 'Modding',
-    links: ['UnderConstruction', 'UnderConstruction']
-  },
+// Horizontal Navigation Menu
+const menuItems = [
+  { name: 'Home', url: 'index.html' },
+  { name: 'About Us', url: 'About Us.html' },
+  { name: 'Guestbook', url: 'http://users3.smartgb.com/g/g.php?a=s&i=g36-37295-9b', external: true },
+  { name: 'Pies Frescos', url: 'Pies Frescos.html' },
+  { name: 'Maidhen', url: 'Maidhen.html' }
 ];
 
-function createSectionHTML(section) {
-  let html = `<h3>${section.title}</h3>`;
-  section.links.forEach(link => {
-    if (link === 'Guestbook') {
-      // Use external URL for Guestbook
-      html += `<p><a href="http://users3.smartgb.com/g/g.php?a=s&i=g36-37295-9b">Guestbook</a></p>`;
+function createHorizontalMenu() {
+  let menuHTML = '<ul>';
+  
+  menuItems.forEach(item => {
+    if (item.external) {
+      menuHTML += `<li><a href="${item.url}" target="_blank" rel="noopener">${item.name}</a></li>`;
     } else {
-      // Use internal links for other sections
-      html += `<p><a href="${link}.html">${link}</a></p>`;
+      menuHTML += `<li><a href="${item.url}">${item.name}</a></li>`;
     }
   });
-  return html;
+  
+  menuHTML += '</ul>';
+  return menuHTML;
 }
 
-let sidebarContent = `
-  <a href="index.html"><h3>Home</h3></a>
-`;
-
-sections.forEach(section => {
-  sidebarContent += createSectionHTML(section);
-});
-
-document.getElementById("sidebar").innerHTML = sidebarContent;
+document.getElementById("sidebar").innerHTML = createHorizontalMenu();
