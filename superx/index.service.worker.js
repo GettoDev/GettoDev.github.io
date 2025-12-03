848b1ceffcad3ba59048e0e62f3c9ba8e09b0532
GettoDev <gettodev@proton.me>
2025-12-03 00:49:41 -0300

    rc

--- superx/index.service.worker.js
+++ superx/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1764731626|226317294';
+const CACHE_VERSION = '1764733529|180206871';
 /** @type {string} */
 const CACHE_PREFIX = 'Supa Loboto X Pr-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
